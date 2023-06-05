c22a8f092ad0efba591081d77dd238d800170cf1
SimonJ09 <jude-simon.agohoundje@epitech.eu>
2023-06-05 16:37:06 +0100

    version

---
+++ django-apps/xage/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Activation de l'environnement virtuel (si applicable)
+source django-apps/env/bin/activate
+
+# Lancement de Gunicorn avec votre application Django
+gunicorn xage.wsgi:application --bind 0.0.0.0:$PORT --workers 4

eefdfa99a54f9b8b49fc8a1b04c8f92fc83bfc7c
SimonJ09 <jude-simon.agohoundje@epitech.eu>
2023-06-06 15:38:23 +0100

    version

--- django-apps/xage/xages/views.py
+++ django-apps/xage/xages/views.py
@@ -7,7 +7,7 @@ import os
 from .models import Utilisateur
 
 def get_sexe_label(prediction):
-    if prediction == 1:
+    if prediction == 0:
         return "féminin"
     else:
         return "masculin"
@@ -24,32 +24,44 @@ def prediction(request):
         # Obtenez le chemin absolu du fichier model.pkl dans le même dossier que views.py
         model_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'model.pkl')
 
-        # Chargez le modèle MultiOutputClassifier à partir du fichier model.pkl
-        model = joblib.load(model_path)
+        try:
+            # Chargez le modèle MultiOutputClassifier à partir du fichier model.pkl
+            model = joblib.load(model_path)
 
-        # Récupérer la photo téléchargée par l'utilisateur
-        photo = request.FILES['photo']
+            # Récupérer la photo téléchargée par l'utilisateur
+            photo = request.FILES['photo']
 
-        # Prétraiter la photo si nécessaire (redimensionnement, normalisation, etc.)
-        img = Image.open(photo)
-        img = img.resize((224, 224))  # Redimensionner l'image à la taille attendue par le modèle
-        img = np.array(img)  # Convertir l'image en tableau NumPy
-        img = img / 255.0  # Normaliser les valeurs des pixels entre 0 et 1
-        img = img.flatten()  # Aplatir l'image en un vecteur 1D
-        img = np.expand_dims(img, axis=0)  # Ajouter une dimension pour représenter le batch
+            # Prétraiter la photo si nécessaire (redimensionnement, normalisation, etc.)
+            img = Image.open(photo)
+            img = img.resize((224, 224))  # Redimensionner l'image à la taille attendue par le modèle
+            img = np.array(img)  # Convertir l'image en tableau NumPy
+            img = img / 255.0  # Normaliser les valeurs des pixels entre 0 et 1
+            img = img.flatten()  # Aplatir l'image en un vecteur 1D
+            img = np.expand_dims(img, axis=0)  # Ajouter une dimension pour représenter le batch
 
-        # Effectuer la prédiction en utilisant le modèle
-        resultat_prediction = model.predict(img)
+            # Effectuer la prédiction en utilisant le modèle
+            resultat_prediction = model.predict(img)
 
-        # Supposons que le résultat de la prédiction est un tableau [[age, sexe]]
-        age = resultat_prediction[0][0]
-        sexe = get_sexe_label(resultat_prediction[0][1])
+            # Supposons que le résultat de la prédiction est un tableau [[age, sexe]]
+            if resultat_prediction.shape == (1, 2):
+                age = resultat_prediction[0][0]
+                sexe = get_sexe_label(resultat_prediction[0][1])
+            else:
+                # Handle the case when the prediction result doesn't have the expected shape
+                age = "Unknown"
+                sexe = "Unknown"
 
-        # Enregistrer les informations dans la base de données
-        #utilisateur = Utilisateur(photo=photo, age=age, sexe=sexe)
-        #utilisateur.save()
+            # Enregistrer les informations dans la base de données
+            # utilisateur = Utilisateur(photo=photo, age=age, sexe=sexe)
+            # utilisateur.save()
 
-        # Renvoyer les résultats de la prédiction à l'interface utilisateur
-        return render(request, 'prediction.html', {'age': age, 'sexe': sexe})
+            # Renvoyer les résultats de la prédiction à l'interface utilisateur
+            return render(request, 'prediction.html', {'age': age, 'sexe': sexe})
+
+        except Exception as e:
+            # Gérer les erreurs lors du chargement du modèle ou de la prédiction
+            error_message = str(e)
+            return render(request, 'prediction.html', {'error': error_message})
+
     else:
         return render(request, 'prediction.html')
